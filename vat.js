d9978e4558b4d5f8e5eee170468756a30bece174
Karim <118430544+Karim08F@users.noreply.github.com>
2024-06-05 11:46:38 +0300

    Add files via upload

---
+++ vat.js
@@ -0,0 +1,52 @@
+const netPrice = document.querySelector("#net-price");
+const vatRate = document.querySelector("#vat-rate");
+const vatAdded = document.querySelector("#vat-added")
+const incAmount = document.querySelector("#inc-amount");
+const calcBtn = document.querySelector("#calc-btn")
+const resetBtn = document.querySelector("#reset-btn")
+
+function VATInclusiveAmount(price, rate) {
+	let vatAddedPrice = Number((price * (rate / 100)));
+	vatAdded.value = vatAddedPrice.toFixed(2);
+	let totalAmount = Number(price) + vatAddedPrice;
+	return totalAmount;
+}
+
+calcBtn.addEventListener('click', function() {
+	if (netPrice.value === "" || isNaN(netPrice.value)) {
+		
+		netPrice.style.border = "1px solid red";
+
+	
+		setTimeout(function() {
+			netPrice.style.border = "1px solid transparent";
+		}, 1500);
+
+		netPrice.value = "";
+	}
+
+	if (vatRate.value === "" || isNaN(vatRate.value)) {
+		
+		vatRate.style.border = "1px solid red";
+
+		
+		setTimeout(function() {
+			vatRate.style.border = "1px solid transparent";
+		}, 1500);
+
+	
+		vatRate.value = "";
+	}
+
+	let finalAmount = VATInclusiveAmount((netPrice.value), (vatRate.value));
+	incAmount.value = finalAmount;
+
+
+})
+
+resetBtn.addEventListener("click", function() {
+	netPrice.value = "";
+	vatRate.value = "";
+	vatAdded.value = "";
+	incAmount.value = "";
+})
